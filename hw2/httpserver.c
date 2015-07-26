a48db8894a246e161139104b15df51c302c5bda8
Anvesh <atanuku91@gmail.com>
2015-07-26 02:04:08 +0000

    proxy server

--- hw2/httpserver.c
+++ hw2/httpserver.c
@@ -14,8 +14,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <unistd.h>
-
+#include <dirent.h>
 #include "libhttp.h"
+
+#define LIBHTTP_REQUEST_MAX_SIZE 8192
 
 /*
  * Global configuration variables.
@@ -29,6 +31,70 @@ char *server_proxy_hostname;
 int server_proxy_port;
 
 /*
+ * Sends file by name line by line
+ */
+void send_file_by_name(char * fname, int fd){
+	char *buffer= 0;
+	long length;
+	FILE * f = fopen(fname,"rb");
+
+	if(f)
+	{
+	  fseek(f,0, SEEK_END);
+	  length = ftell(f);
+	  fseek(f, 0, SEEK_SET);
+	  buffer = malloc(length);
+          if (buffer)
+	   {
+            fread(buffer, 1, length, f);
+           }
+           fclose(f);
+
+	}
+
+	if(buffer){
+	   http_start_response(fd, 200);
+  	   http_send_header(fd, "Content-type", http_get_mime_type(fname) );
+  	   http_end_headers(fd);
+	   http_send_data(fd, buffer, length);
+	   free(buffer);
+        }
+	
+  	
+
+
+}
+
+
+void list_files_in_dir(char * dir_name, int fd){
+
+	http_start_response(fd, 200);
+  	http_send_header(fd, "Content-type", "text/html");
+  	http_end_headers(fd);
+	char send_buf[2048];
+	http_send_string(fd, "<!DOCTYPE html>\n");
+	http_send_string(fd, "<html>\n");
+	http_send_string(fd, "<body>\n");
+	DIR *dir = opendir(dir_name);
+		struct dirent *entry;
+		while((entry = readdir(dir)) != NULL)
+		{
+			sprintf(send_buf,"<br> <a href=\"http://192.168.162.162:8000/%s\"> %s </a>",entry->d_name, entry->d_name); 
+			http_send_string(fd, send_buf);
+		}
+	http_send_string(fd,"</body>\n");
+	http_send_string(fd,"</html>\n");
+	closedir(dir);
+}
+
+void send_404(int fd){
+
+  http_start_response(fd, 200);
+  http_send_header(fd, "Content-type", "text/html");
+  http_end_headers(fd);
+  http_send_string(fd, "<center><h1>http Error 404.</h1><hr><p>File not found.</p></center>");
+}
+/*
  * Reads an HTTP request from stream (fd), and writes an HTTP response
  * containing:
  *
@@ -41,19 +107,97 @@ int server_proxy_port;
  */
 void handle_files_request(int fd)
 {
-
-  /* YOUR CODE HERE */
+  int found = 0;
+  char working_path[2048];
+ 
 
   struct http_request *request = http_request_parse(fd);
+   
+  if(request->path[0] == '/'){
+	printf("method: %s, path: %s\n", request->method, request->path);
+  	printf("serving out of %s\n", server_files_directory); 
+	sprintf(working_path, "%s%s", server_files_directory, request->path);
+	printf("constructed path: %s\n",working_path); 	
+	struct stat st;
+	stat(working_path, &st);
+	if(S_ISDIR(st.st_mode)){
+		found = 0;
+		DIR *dir = opendir(working_path);
+		struct dirent *entry;
+		while((entry = readdir(dir)) != NULL)
+		{
+			if (strcmp(entry->d_name, "index.html") == 0){
+				found = 1;
+				char file_to_send[2048];
+				sprintf(file_to_send, "%s/%s",working_path,"index.html");
+				send_file_by_name(file_to_send, fd);	
+			}
+		}
+		closedir(dir);
+		if(found == 0){
+			list_files_in_dir(working_path, fd);
+		}			
+	}
+	else if (S_ISREG(st.st_mode)){
+		send_file_by_name(working_path, fd);
+	}
+	else{
+		send_404(fd);
+	}
+  }
 
-  http_start_response(fd, 200);
-  http_send_header(fd, "Content-type", "text/html");
-  http_end_headers(fd);
-  http_send_string(fd, "<center><h1>Welcome to httpserver!</h1><hr><p>Nothing's here yet.</p></center>");
+
+//  http_start_response(fd, 200);
+//  http_send_header(fd, "Content-type", "text/html");
+//  http_end_headers(fd);
+//  http_send_string(fd, "<center><h1>Welcome to httpserver!</h1><hr><p>Nothing's here yet.</p></center>");
 
 }
 
-/*
+int proxy_request(int fd, int fd_send){
+ 
+ char *read_buffer = malloc(LIBHTTP_REQUEST_MAX_SIZE + 1);
+  if (!read_buffer) http_fatal_error("Malloc failed");
+
+  int bytes_read = read(fd, read_buffer, LIBHTTP_REQUEST_MAX_SIZE);
+ 
+  int bytes_sent = write(fd_send, read_buffer, bytes_read);
+
+  
+
+  if(bytes_read == bytes_sent)
+	printf("everything sent\n");
+  else
+	printf("bytes read: %d, bytes recieved %d\n", bytes_read, bytes_sent);
+
+
+  return bytes_sent;
+}
+ 
+void init_sockaddr (struct sockaddr_in *name, const char *hostname, uint16_t port){
+	memset(name, 0, sizeof(*name));
+
+	struct hostent *hostinfo;
+	
+	name->sin_family = AF_INET;
+	
+	name->sin_port = htons(port);
+	
+	hostinfo = gethostbyname(hostname);
+	
+	if(hostinfo == NULL)
+		{
+		  fprintf(stderr, "Unknown host %s.\n", hostname);
+		  exit( EXIT_FAILURE);
+		}
+
+	name->sin_addr = *(struct in_addr *) hostinfo->h_addr;
+	
+}
+
+
+
+/*			
  * Opens a connection to the proxy target (hostname=server_proxy_hostname and
  * port=server_proxy_port) and relays traffic to/from the stream fd and the
  * proxy target. HTTP requests from the client (fd) should be sent to the
@@ -66,9 +210,113 @@ void handle_files_request(int fd)
  */
 void handle_proxy_request(int fd)
 {
-
+  printf("printing\n");
+  char *p;
   /* YOUR CODE HERE */
+//  p = dns_lookup(server_proxy_hostname);
+  struct sockaddr_in name;
+  //name = (struct sockaddr_in *)malloc(sizeof(struct sockadd_in)); 
+  init_sockaddr(&name, server_proxy_hostname, server_proxy_port);
+  
+  int* socket_number_proxy;
+  socket_number_proxy = (int *) malloc(sizeof(int));
+  *socket_number_proxy = socket(PF_INET, SOCK_STREAM, 0);
+  
+  printf("proxy socket number %d\n", *socket_number_proxy);
+  if(*socket_number_proxy == -1){
+	fprintf(stderr, "failed to create new proxy socket: error %d: %s\n", errno, strerror(errno));
+  	exit(errno);
+   }
+  
+  printf("made proxy socket\n");
+   int socket_option = 1;
 
+   if(connect(*socket_number_proxy, (struct sockaddr*)&name, sizeof(name))){
+	printf("error");
+   }
+  printf("Listening on port %d...\n", server_proxy_port);
+  
+  //get max file descriptor for select call
+  int maxfd = (fd> *socket_number_proxy ? fd : *socket_number_proxy);
+
+  //initalize read and write sets to pass to select call and clear them 
+  fd_set readset;
+  fd_set writeset;
+  FD_ZERO(&readset);
+  FD_ZERO(&writeset);
+
+  //add the client file descriptors to read and write sets for select
+  FD_SET(fd, &readset);
+  FD_SET(fd, &writeset);
+
+  //do same for remote server file descriptor
+  FD_SET(*socket_number_proxy, &readset);
+  FD_SET(*socket_number_proxy, &writeset);
+
+  
+  //temporary sets for polling i guess
+  fd_set readtemp;
+  fd_set writetemp;
+
+  int result;
+
+  printf("max fd %d\n", maxfd);
+  int socketsopen = 1;
+  int bytes_sent = 0;
+  while(socketsopen){
+//	printf("inside outer while loop\n");
+        memcpy(&readtemp, &readset, sizeof(readtemp));
+        memcpy(&writetemp, &writeset, sizeof(writetemp));
+
+  	result = select(maxfd + 1, &readtemp, NULL, NULL, NULL);
+	
+		
+	if(result == -1){
+		printf("something bad happened\n");
+	}
+	else{
+		printf("whats up doc?\n");
+		if(FD_ISSET(fd, &readtemp)){
+			printf("case 1\n");
+			bytes_sent = proxy_request(fd, *socket_number_proxy);
+		//	proxy_request(*socket_number_proxy, fd);
+
+		}
+		else if(FD_ISSET(*socket_number_proxy, &readtemp)){
+			printf("case 2\n");
+			bytes_sent = proxy_request(*socket_number_proxy, fd);
+		//	proxy_request(fd, *socket_number_proxy);
+		}
+	}
+	if (bytes_sent < 0){
+		socketsopen = 0;
+	}
+	
+
+  }
+		
+		
+
+}
+
+int socket_isalive(int fd){
+	char c;
+	int alive = 0;
+	int x = 4;
+    //    ssize_t x = recv(fd, &c, 1, MSG_PEEK);
+	if(x > 0){
+		alive = 1;
+	}
+	else if (x == 0){
+		alive = 0;
+	}
+	else{
+		//pretend the socket is dead for now 
+		//i don't know how else this can be handled yet
+		alive = 0;
+	}
+	
+	return alive;
 }
 
 /*
